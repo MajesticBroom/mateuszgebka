2b1d9a18cdd71df48f4e1d3413ac6fd98d0bfc3f
MajesticBroom <mateuszgebka2@gmail.com>
2024-02-19 00:11:06 +0100

    commit #4 content

--- light-dark-mode.js
+++ light-dark-mode.js
@@ -1,17 +1,19 @@
 let mode = document.querySelector(".light-dark-mode-icon");
-let bg = document.querySelector("body");
+let body = document.querySelector("body");
 let logo = document.querySelector(".logo");
 
 function changeToLight () {
     mode.src = "./icons/moon-svgrepo-com-darkblue.svg";
     mode.setAttribute("onclick", "changeToDark()");
-    bg.style.backgroundColor = "rgb(206, 217, 230)";
+    body.style.backgroundColor = "rgb(206, 217, 230)";
+    body.style.color = "rgb(16, 32, 47)";
     logo.style.color = "rgb(16, 32, 47)";
 }
 
 function changeToDark () {
     mode.src = "./icons/sun-svgrepo-com-lightblue.svg";
     mode.setAttribute("onclick", "(changeToLight())");
-    bg.style.backgroundColor = "rgb(16, 32, 47)";
+    body.style.backgroundColor = "rgb(16, 32, 47)";
+    body.style.color = "rgb(206, 217, 230)";
     logo.style.color = "rgb(206, 217, 230)";
 }
