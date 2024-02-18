958b8442057519f947ea4a6ee27672541c9bc42f
MajesticBroom <mateuszgebka2@gmail.com>
2024-02-19 00:23:53 +0100

    commit #6 update js

--- light-dark-mode.js
+++ light-dark-mode.js
@@ -1,9 +1,15 @@
 let mode = document.querySelector(".light-dark-mode-icon");
 let body = document.querySelector("body");
 let logo = document.querySelector(".logo");
+let fb = document.querySelector(".facebook");
+let ig = document.querySelector(".instagram");
+let gh = document.querySelector(".github");
 
 function changeToLight () {
     mode.src = "./icons/moon-svgrepo-com-darkblue.svg";
+    fb.src = "./icons/icons8-facebook-darkblue.svg";
+    ig.src = "./icons/icons8-instagram-darkblue.svg";
+    gh.src = "./icons/icons8-github-darkblue.svg";
     mode.setAttribute("onclick", "changeToDark()");
     body.style.backgroundColor = "rgb(206, 217, 230)";
     body.style.color = "rgb(16, 32, 47)";
@@ -13,6 +19,9 @@ function changeToLight () {
 function changeToDark () {
     mode.src = "./icons/sun-svgrepo-com-lightblue.svg";
     mode.setAttribute("onclick", "(changeToLight())");
+    fb.src = "./icons/icons8-facebook-lightblue.svg";
+    ig.src = "./icons/icons8-instagram-lightblue.svg";
+    gh.src = "./icons/icons8-github-lightblue.svg";
     body.style.backgroundColor = "rgb(16, 32, 47)";
     body.style.color = "rgb(206, 217, 230)";
     logo.style.color = "rgb(206, 217, 230)";
